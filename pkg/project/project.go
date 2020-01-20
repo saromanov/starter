a0af19e33cdb4713b91d23e2a70ec9ae5b094b69
Sergey <xxsmotur@gmail.com>
2020-01-20 21:06:53 +0500

    Add method fo create dockerfile

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -3,6 +3,7 @@ package project
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/saromanov/starter/pkg/models"
@@ -12,6 +13,12 @@ var errNoName = errors.New("name of the project is not defined")
 
 // Build provides building of the project
 func Build(p *models.Project) error {
+	if err := makeDirs(p); err != nil {
+		return err
+	}
+	if err := createDockerfile(p); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -28,6 +35,20 @@ func makeDirs(p *models.Project) error {
 		if err := os.Mkdir(p.Name, os.ModeDir); err != nil {
 			return fmt.Errorf("unable to create dir %s dir: %v", d, err)
 		}
+	}
+
+	return nil
+}
+
+func createDockerfile(p *models.Project) error {
+	if !p.Dockerfile {
+		return nil
+	}
+
+	d1 := []byte("FROM golang:1.13")
+	err := ioutil.WriteFile("Dockerfile", d1, 0644)
+	if err != nil {
+		return fmt.Errorf("unable to create dockerfile: %v", err)
 	}
 
 	return nil
