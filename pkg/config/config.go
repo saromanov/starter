5091fe7355f1dbf038e1e14f720936f7cb4f6ec9
Sergey <xxsmotur@gmail.com>
2020-01-23 00:05:55 +0500

    fill of teh config package

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,1 +1,30 @@
 package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/saromanov/cowrow"
+)
+
+// Config provides definition of the project
+type Config struct {
+	Name       string   `yaml:"name"`
+	Author     string   `yaml:"author"`
+	GitPath    string   `yaml:"gitpath"`
+	Dockerfile string   `yaml:"dockerfile"`
+	Makefile   string   `yaml:"makefile"`
+	SubDirs    []string `yaml:"subdirs"`
+}
+
+// Load provides loading of the config
+func Load(path string) (*Config, error) {
+
+	c := &Config{}
+	if err := cowrow.LoadByPath(path, &c); err != nil {
+		return nil, fmt.Errorf("unable to load config: %v", err)
+	}
+	
+	return c, nil
+}
+
