2143ad5e2f352d72af0a794e780b5648856d969e
Sergey <xxsmotur@gmail.com>
2020-01-22 23:31:52 +0500

    fix typo

--- pkg/project/files.go
+++ pkg/project/files.go
@@ -3,8 +3,6 @@ package project
 import (
 	"fmt"
 	"io/ioutil"
-
-	"github.com/saromanov/starter/pkg/models"
 )
 
 // moveFile provides copy of the target file
@@ -12,12 +10,12 @@ import (
 func moveFile(inFile, outFile string) error {
 	input, err := ioutil.ReadFile(inFile)
 	if err != nil {
-		return fmt.Errorf("unable to open Dockerfile: %s %v", inFile, err)
+		return fmt.Errorf("unable to open file: %s %v", inFile, err)
 	}
 
 	err = ioutil.WriteFile(outFile, input, 0644)
 	if err != nil {
-		return fmt.Errorf("unable to create Dockerfile at: %s %v", p.Name, err)
+		return fmt.Errorf("unable to create file at: %s %v", outFile, err)
 	}
 
 	return nil
