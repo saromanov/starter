fe8ac07ae2f62d25beb8d0743cfb0b34c0b58be0
Sergey <xxsmotur@gmail.com>
2020-02-03 16:19:18 +0500

    Using of correct entry file name

--- pkg/task/readme/readme.go
+++ pkg/task/readme/readme.go
@@ -31,7 +31,10 @@ func (d *Readme) String() string {
 
 // Do defines action of the task
 func (d *Readme) Do() error {
-	d1 := []byte(generate(d.p.Name, d.p.Description, d.p.Author))
+	if d.p.EntryFile == "" {
+		return nil
+	}
+	d1 := []byte(generate(d.p.EntryFile, d.p.Description, d.p.Author))
 	err := ioutil.WriteFile(fmt.Sprintf("%s/README.md", d.p.Name), d1, 0644)
 	if err != nil {
 		return fmt.Errorf("unable to write readme: %v", err)
