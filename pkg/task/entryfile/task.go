f8b5f9042006b3f808fe0e8def4e5ec38ce94d91
Sergey <xxsmotur@gmail.com>
2020-04-06 18:21:29 +0500

    Entryfile task: Creating of subdirs

--- pkg/task/entryfile/task.go
+++ pkg/task/entryfile/task.go
@@ -3,6 +3,8 @@ package entryfile
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path"
 	"strings"
 
 	"github.com/saromanov/starter/pkg/models"
@@ -31,6 +33,9 @@ func (d *Entryfile) Do() error {
 	if d.p.EntryFile == "" || d.p.EntryFile == "\n" {
 		return nil
 	}
+	if err := d.createSubDirs(); err != nil {
+		return fmt.Errorf("unable to create subdirs: %v", err)
+	}
 	d1 := []byte(fmt.Sprintf("package %s", d.p.Name))
 	fileName := d.getFileName()
 	err := ioutil.WriteFile(fileName, d1, 0644)
@@ -38,6 +43,16 @@ func (d *Entryfile) Do() error {
 		return fmt.Errorf("unable to write file %s: %v", fileName, err)
 	}
 	return nil
+}
+
+// createSubDirs provides checking of the file
+// if inout data contains subdirs, then create it
+func (d *Entryfile) createSubDirs() error {
+	subDirs := path.Dir(d.p.EntryFile)
+	if subDirs == "." {
+		return nil
+	}
+	return os.MkdirAll(subDirs, 0777)
 }
 
 // getFileName returns result file name
