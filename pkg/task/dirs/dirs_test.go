71b967f11127c7b36974a95d8017293e17d92c39
Sergey <xxsmotur@gmail.com>
2020-04-07 23:50:43 +0500

    Add method for remove test dir

--- pkg/task/dirs/dirs_test.go
+++ pkg/task/dirs/dirs_test.go
@@ -1,6 +1,8 @@
 package dirs
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -8,6 +10,26 @@ import (
 
 var rootTestDir = "./tmp"
 
+func removeContentFromRoot(dir string) error {
+	d, err := os.Open(dir)
+	if err != nil {
+		return err
+	}
+	defer d.Close()
+	names, err := d.Readdirnames(-1)
+	if err != nil {
+		return err
+	}
+	for _, name := range names {
+		err = os.RemoveAll(filepath.Join(dir, name))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func TestMakeRootDir(t *testing.T) {
 	assert.NoError(t, makeRootDir(rootTestDir))
+	assert.NoError(t, removeContentFromRoot(rootTestDir))
 }
