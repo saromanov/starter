5d5374b64163e60b0399ffdcd1407570215a7f04
Sergey <xxsmotur@gmail.com>
2020-04-15 15:43:44 +0500

    Adding tests for git remote

--- pkg/task/gitremote/task_test.go
+++ pkg/task/gitremote/task_test.go
@@ -3,10 +3,24 @@ package gitremote
 import (
 	"testing"
 
+	"github.com/saromanov/starter/pkg/models"
 	"github.com/stretchr/testify/assert"
 )
+
+const remotePath = "https://github.com/saromanov/starter"
 
 func TestDo(t *testing.T) {
 	p := New(nil)
 	assert.Nil(t, p.Do())
+
+	p = New(&models.Project{
+		Name: "foobar",
+	})
+	assert.Nil(t, p.Do())
+
+	p = New(&models.Project{
+		Name:    "foobar",
+		GitPath: remotePath,
+	})
+	assert.NotNil(t, p.Do())
 }
